fc48baa69cf23e9a99a731410bacb6da599ef777
Rajat Bansal <18ucs048@gmail.com>
2021-06-24 18:34:23 +0530

    ; removed

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db'); 
-const PORT = process.env.PORT || 3000
+
 
 connectDB();
+app.use(express.json());
 
 app.use('/api/files', require('./routes/file'))
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
