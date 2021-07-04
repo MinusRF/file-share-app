5178aabebfb790b893ea876fff83f6833313a796
Rajat Bansal <18ucs048@gmail.com>
2021-07-04 18:41:06 +0530

    commit

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const app = express();
-const connectDB = require('./config/db'); 
+const path = require('path');
 
-
-connectDB();
-app.use(express.json());
-
-app.use('/api/files', require('./routes/file'))
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+// connect database.
+const connectDB = require('./config/db'); 
+connectDB();
+
+// template engine.
+app.set('views', path.join(__dirname, '/views' ));
+app.set('view engine', 'ejs');
+
+
+app.use(express.json());
+
+
+// routes.
+
+app.use('/api/files', require('./routes/file'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
+
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
