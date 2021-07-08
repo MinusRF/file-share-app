8c92265012165afe05d3a68a773624e156bebbdc
Rajat Bansal <18ucs048@gmail.com>
2021-07-08 10:04:17 +0530

    updated emailTemplate

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -131,8 +131,8 @@ module.exports = ({ emailFrom, downloadLink, size, expires }) => {
                                         </tr>
                                     </tbody>
                                     </table>
-                                    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Thank you for using inShare service.</p>
-                                    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Good luck! Hope it works.</p>
+                                    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Thank you for using ShareAnywhere service.</p>
+                                    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Have a nice day!</p>
                                 </td>
                                 </tr>
                             </table>
