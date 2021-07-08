fc41ed7971afd140d2c81c5429cd21e4f8292e56
Rajat Bansal <18ucs048@gmail.com>
2021-07-08 10:07:57 +0530

    updated emailTemplate

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -147,8 +147,8 @@ module.exports = ({ emailFrom, downloadLink, size, expires }) => {
                         <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
                             <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
-                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">inShare inc. main street 29, Moscow</span>
-                                <br> Want to share a file? <a href="http://localhost:3000" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">inShare</a>.
+                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">ShareAnywhere inc. main street 29, Jaipur</span>
+                                <br> Want to share a file? <a href="http://localhost:3000" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">ShareAnywhere</a>.
                             </td>
                             </tr>
                             <tr>
